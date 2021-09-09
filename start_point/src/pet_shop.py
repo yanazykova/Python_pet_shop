4700006fa41fd85c03067ea268a784d241869d6f
yanazykova <yvz1973@gmail.com>
2021-09-09 13:13:11 +0100

    first commit

---
+++ start_point/src/pet_shop.py
@@ -0,0 +1,35 @@
+# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(shop):
+    return shop["name"]
+
+def get_total_cash(shop):
+    return shop["admin"]["total_cash"]
+
+def add_or_remove_cash(shop, amount):
+    shop["admin"]["total_cash"] += amount
+
+
+def get_pets_sold(shop):  
+    return shop["admin"]["pets_sold"]
+
+
+def increase_pets_sold(shop, number):
+     shop["admin"]["pets_sold"] += number
+
+
+def get_stock_count(shop):
+    return len(shop["pets"])
+
+def get_pets_by_breed(shop, breed):
+    found = []
+    for pet in shop["pets"]:
+        if pet["breed"] == breed:
+            found.append(pet)
+    return found
+
+def find_pet_by_name(shop, name):
+    for pet in shop["pets"]:
+        if pet["name"] == name:
+            return pet
+
+def
